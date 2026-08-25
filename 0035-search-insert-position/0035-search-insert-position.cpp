acf9cc7bc79757675e22c286e029a5d1bb0e5f69
MOHAMMAD SIDDIQ SHAIK <skmsiddiqsiddiq@gmail.com>
2026-08-26 02:47:31 +0530

    Time: 0 ms (100%), Space: 13.5 MB (98.67%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        if (target <= nums[0])
+         return 0;
+         
+        for(int i =0;i<nums.size()-1;i++){
+            if(nums[i]==target){
+                return i;
+            }
+            else if (nums[i]<target && target<=nums[i+1]){
+              return i+1;
+            }
+        }
+        return nums.size();
+    }
+};
